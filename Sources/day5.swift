2912ed9717210fbb11a990403aa1d7752a80a0f4
Yingbai He <yingbai.he93@gmail.com>
2016-12-14 04:14:21 -0800

    day5 part2

--- Sources/day5.swift
+++ Sources/day5.swift
@@ -6,7 +6,7 @@ class Day5: Day {
     let input = "cxdnnyjw"
 
     print("Answer for part1: \(part1(input: input))")
-    // print("Answer for part2: \(part2(lines: lines))")
+    print("Answer for part2: \(part2(input: input))")
   }
 
   func part1(input: String) -> String {
@@ -26,7 +26,31 @@ class Day5: Day {
     return ""
   }
 
-  func part2(lines: [String]) -> Int {
-    return 0
+  func part2(input: String) -> String {
+    var count = 0
+    var index = 0
+    var password: [Int:String] = [:]
+    while true {
+      let output = (input + String(index)).md5()
+      let position = output.index(output.startIndex, offsetBy: 5)
+      let num = output.index(output.startIndex, offsetBy: 6)
+      if output.hasPrefix("00000") {
+        if let positionNum = Int(String(output[position])) {
+          if 0...7 ~= positionNum {
+            guard let _ = password[positionNum] else {
+              password[positionNum] = String(output[num])
+              print(output, index, String(output[num]))
+              count += 1
+              continue
+            }
+          }
+        }
+      }
+      if count >= 8 {
+        break
+      }
+      index += 1
+    }
+    return ""
   }
 }
